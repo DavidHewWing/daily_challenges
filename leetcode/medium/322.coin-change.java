aefbdbb421dcb890be9b80c1fa373ebad37870b4
David Hew-wing <dhew67@gmail.com>
2019-10-21 21:47:06 -0400

    two

--- leetcode/medium/322.coin-change.java
+++ leetcode/medium/322.coin-change.java
@@ -7,7 +7,20 @@
 // @lc code=start
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        
+        int n = amount + 1;
+        int[] dp = new int[n];
+        int min = n;
+        Arrays.fill(dp, min);
+        dp[0] = 0;
+        for(int i = 1; i < n; i++) {
+            for(int j = 0; j < coins.length; j++) {
+                int value = i - coins[j];
+                if (coins[j] <= i) {
+                    dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
+                }
+            }
+        }
+        return dp[amount] > amount ? -1 : dp[amount];
     }
 }
 // @lc code=end
