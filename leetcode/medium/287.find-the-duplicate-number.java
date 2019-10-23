693ee15f5e3d006e255d7a1c5a8ee6a8197b9e3b
David Hew-wing <dhew67@gmail.com>
2019-10-22 22:59:38 -0400

    287

---
+++ leetcode/medium/287.find-the-duplicate-number.java
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode id=287 lang=java
+ *
+ * [287] Find the Duplicate Number
+ */
+
+// @lc code=start
+class Solution {
+    public int findDuplicate(int[] nums) {
+        int tortoise = nums[0];
+        int hare = nums[0];
+        do {
+            tortoise = nums[tortoise];
+            hare = nums[nums[hare]];
+        } while (tortoise != hare);
+
+        // Find the "entrance" to the cycle.
+        int ptr1 = nums[0];
+        int ptr2 = tortoise;
+        while (ptr1 != ptr2) {
+            ptr1 = nums[ptr1];
+            ptr2 = nums[ptr2];
+        }
+
+        return ptr1;
+    }
+}
+// @lc code=end
+
