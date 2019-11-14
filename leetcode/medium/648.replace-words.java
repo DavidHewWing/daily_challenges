769a00efb4ef4d0c7911203b39b91943eac55d7a
David Hew-wing <dhew67@gmail.com>
2019-11-13 22:43:06 -0500

    replace words

---
+++ leetcode/medium/648.replace-words.java
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode id=648 lang=java
+ *
+ * [648] Replace Words
+ */
+
+// @lc code=start
+class Solution {
+    public String replaceWords(List<String> dict, String sentence) {
+        if (dict == null || dict.size() == 0) return sentence;
+        
+        Set<String> set = new HashSet<>();
+        for (String s : dict) set.add(s);
+        
+        StringBuilder sb = new StringBuilder();
+        String[] words = sentence.split("\\s+");
+        
+        for (String word : words) {
+            String prefix = "";
+            for (int i = 1; i <= word.length(); i++) {
+                prefix = word.substring(0, i);
+                if (set.contains(prefix)) break;
+            }
+            sb.append(" " + prefix);
+        }
+        
+        return sb.deleteCharAt(0).toString();
+    }
+}
+// @lc code=end
+
