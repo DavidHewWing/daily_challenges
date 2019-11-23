c5ccc9700790994d264c825bbb8c3ce25645f6b0
David Hew-wing <dhew67@gmail.com>
2019-11-22 20:56:00 -0500

    easy

---
+++ leetcode/easy/566.reshape-the-matrix.java
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode id=566 lang=java
+ *
+ * [566] Reshape the Matrix
+ */
+
+// @lc code=start
+class Solution {
+    public int[][] matrixReshape(int[][] nums, int r, int c) {
+        int[][] res = new int[r][c];
+        if (nums.length == 0 || r * c != nums.length * nums[0].length)
+            return nums;
+        int row = 0;
+        int col = 0;
+        for(int i = 0; i < nums.length; i++){
+            for(int j = 0; j < nums[0].length; j++){
+                res[row][col] = nums[i][j];
+                col++;
+                if(col == c){
+                    col = 0;
+                    row++;
+                }
+            }
+        }
+        return res;
+    }
+}
+// @lc code=end
+
