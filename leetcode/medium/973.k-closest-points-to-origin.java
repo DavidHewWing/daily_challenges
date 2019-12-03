c036eb367c346bcde8166e606af1cd022d43b801
David Hew-wing <dhew67@gmail.com>
2019-12-02 19:13:12 -0500

    medium

---
+++ leetcode/medium/973.k-closest-points-to-origin.java
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode id=973 lang=java
+ *
+ * [973] K Closest Points to Origin
+ */
+
+// @lc code=start
+class Solution {
+    public int[][] kClosest(int[][] points, int K) {
+        PriorityQueue<int[]> pq = new PriorityQueue<int[]>((p1, p2) -> p2[0] * p2[0] + p2[1] * p2[1] - p1[0] * p1[0] - p1[1] * p1[1]);
+    for (int[] p : points) {
+        pq.offer(p);
+        if (pq.size() > K) {
+            pq.poll();
+        }
+    }
+    int[][] res = new int[K][2];
+    while (K > 0) {
+        res[--K] = pq.poll();
+    }
+    return res;
+    
+}
+}
+// @lc code=end
+
