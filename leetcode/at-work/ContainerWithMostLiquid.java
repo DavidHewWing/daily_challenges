cd014629ada6e4a477be6040fe374ee780d4563d
David Hew-wing <32623283+DavidHewWing@users.noreply.github.com>
2019-12-12 15:48:09 -0500

    Create ContainerWithMostLiquid.java

---
+++ leetcode/at-work/ContainerWithMostLiquid.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+        int max = Integer.MIN_VALUE;
+        while(left < right){
+            int minHeight = Math.min(height[left], height[right]);
+            max = Math.max(max, minHeight * (right - left));
+            if(height[left] > height[right]){
+                right--;
+            }else{
+                left++;
+            }
+        }
+        return max;
+    }
+}
