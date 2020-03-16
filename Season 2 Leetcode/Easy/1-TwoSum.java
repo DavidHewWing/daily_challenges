693db722929f77e465bedfd9205f21ce643c0742
David Hew-wing <dhew67@gmail.com>
2020-03-16 16:10:18 -0400

    added an easy question

---
+++ Season 2 Leetcode/Easy/1-TwoSum.java
@@ -0,0 +1,18 @@
+class Solution {
+  public int[] twoSum(int[] nums, int target) {
+      HashMap<Integer, Integer> map = new HashMap<>();
+      for (int i = 0; i < nums.length; i++) {
+          if (map.containsKey(nums[i]))
+              return new int[]{i, map.get(nums[i])};
+          map.put(target-nums[i], i);
+      }
+      return new int[]{0,0};
+  }
+}
+
+/**
+ * Easy question - Getting back into things.
+ * Basically all you do is put the complement into a HashMap.
+ * This way when you see the complement in the map, you know you have already seen the addiditive as well.
+ * Thus you can now return the value.
+ */
