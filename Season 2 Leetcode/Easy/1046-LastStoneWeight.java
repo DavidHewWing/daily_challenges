1ccf47c5c8317b2b294981fd1adb27c3fbbcba6e
David Hew-wing <dhew67@gmail.com>
2020-04-13 16:05:11 -0400

    finished easy

---
+++ Season 2 Leetcode/Easy/1046-LastStoneWeight.java
@@ -0,0 +1,18 @@
+package Easy;
+
+class Solution {
+  public int lastStoneWeight(int[] stones) {
+      Queue<Integer> queue = new PriorityQueue<Integer>((a,b) -> b - a);
+      for (int i : stones) queue.offer(i);
+      while (queue.size() > 1) {
+          int first = queue.poll();
+          int second = queue.poll();
+          if (first > second) {
+              queue.offer(first - second);
+          } else if (second > first) {
+              queue.offer(second - first);
+          }
+      }
+      return (queue.isEmpty()) ? 0 : queue.poll();
+  }
+}
